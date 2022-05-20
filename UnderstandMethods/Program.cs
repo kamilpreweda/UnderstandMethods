a536f74e7e030f0d9acfeeebaa0d5842ccb0c9b5
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-20 15:46:36 +0200

    Dodaj pliki projektów.

---
+++ UnderstandMethods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Abracadabra
+{
+    public int Vavavoom;
+
+    public bool Lala(int floq)
+    {
+        if (floq < Vavavoom)
+        {
+            Vavavoom += floq;
+            return true;
+        }
+        return false;
+    }
+}
+class Pizzazz
+{
+    public int Zippo;
+
+    public void Bamboo(int eek)
+    {
+        Zippo += eek;
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pizzazz foxtrot = new Pizzazz() { Zippo = 2 };
+        foxtrot.Bamboo(foxtrot.Zippo);
+        Pizzazz november = new Pizzazz() { Zippo = 3 };
+        Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
+        while (tango.Lala(november.Zippo))
+        {
+            november.Zippo *= -1;
+            november.Bamboo(tango.Vavavoom);
+            foxtrot.Bamboo(november.Zippo);
+            tango.Vavavoom -= foxtrot.Zippo;
+        }
+        Console.WriteLine("november.Zippo = " + november.Zippo);
+        Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
+        Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
+    }
+}
